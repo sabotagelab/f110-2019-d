f077693ff5e5c7bb1359da86f24a4956f97d7606
osboxes.org <bees_inthegarden@yahoo.co.in>
2019-04-10 01:37:56 -0400

    q1

---
+++ amrita_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/osboxes/catkin_ws/src
+# Build directory: /home/osboxes/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(beginner_tutorials)
