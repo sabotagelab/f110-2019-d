49cf9c6ce5a958550589173708803be60e5a0233
Brian Layng <layngb@oregonstate.edu>
2019-04-13 19:38:40 -0700

    added c++ code equivalent to the find_k python code

--- pemdas_gap_finding/src/pemdas_gap_finding.cpp
+++ pemdas_gap_finding/src/pemdas_gap_finding.cpp
@@ -5,6 +5,10 @@
 #include "ros/ros.h"
 #include "sensor_msgs/LaserScan.h"
 #include "geometry_msgs/Point.h"
+#include <array>
+#include <vector>
+#include "std_msgs/Int32.h"
+#include "std_msgs/Int32MultiArray.h"
 
 class SubscribeAndPublish
 {
@@ -12,29 +16,64 @@ public:
   SubscribeAndPublish()
   { 
     //Topic you want to publish
-    pub_ = n_.advertise<geometry_msgs::Point>("/gap_center", 10);
+    pub_ = n_.advertise<std_msgs::Int32>("/features", 100);
     //Topic you want to subscribe
-    sub_ = n_.subscribe("/scan", 10, &SubscribeAndPublish::callback, this);
+    sub_ = n_.subscribe("/scan", 100, &SubscribeAndPublish::callback, this);
   }
 
-  void callback(const sensor_msgs::LaserScan& input)
-  {
-    
+  void callback(const sensor_msgs::LaserScan& scan)
+	{
+  
+	this->scansize = scan.ranges.size();
+	this->thres = 15000;
+	// float input[scan.ranges.size()] = {scan.ranges};
+	float input[scan.ranges.size()];
+	for (int i = 0;i<this->scansize;i++) input[i] = scan.ranges[i];
+	double dscan[this->scansize];
+	double value;
+	int pre_k = 1;
+	std_msgs::Int32 k;
+	std::vector<int> feature;	
 
+	// checks if any scan ranges are infinite or NaN
+	for(int i = 0; i < scansize; i++){
+		if (std::isnan(input[i]) || std::isinf(input[i])){
+			input[i] = scan.range_max + 1;
+		}
+	}
 
-    geometry_msgs::Point output;
-    output.z = 0;
-    pub_.publish(output);
+	// Take first gradient of laser range data (rate of change of information)
+	dscan[0] =(input[0] - input[1])/scan.angle_increment;
+	dscan[scansize-1] = (input[scansize-1] - input[scansize-2])/scan.angle_increment;
+	for(int i = 1; i < scansize-1; i++){
+		dscan[i] = (.5*(input[i-1] - input[i+1]))/scan.angle_increment;
+	}
 
-    // ROS_INFO("I heard: [%f]", output);
+	// Take second gradient of laser range data (spikes indicate features)
+	if (fabs(dscan[0] - dscan[1])/scan.angle_increment > this->thres) feature.push_back(0);
+	for(int i = 1; i < scansize-1; i++){
+		value = fabs(.5*dscan[i-1] - dscan[i+1])/scan.angle_increment;
+		if (value > this->thres) feature.push_back(i);
+		// ROS_INFO("I heard: [%f]", value);
+	}
+	if (fabs(dscan[0] - dscan[1])/scan.angle_increment > this->thres) feature.push_back(scansize-1);
+	ROS_INFO("Features at:");
+	for (int i = 0; i < feature.size(); i++){
+		ROS_INFO("[%i]",feature[i]);
+		if (feature[i+1]-feature[i] > 1) pre_k++;
+	}
+	k.data = pre_k;
+
+    pub_.publish(k);
+
   }
 
 private:
   ros::NodeHandle n_; 
   ros::Publisher pub_;
   ros::Subscriber sub_;
-  double window[10];
-  int iteration;
+  int scansize;
+  double thres; 
 };//End of class SubscribeAndPublish
 
 int main(int argc, char **argv)
@@ -49,47 +88,4 @@ int main(int argc, char **argv)
 
   return 0;
 }
-/**
- * This tutorial demonstrates simple receipt of messages over the ROS system.
- */
-// %Tag(CALLBACK)%
-// void chatterCallback(const geometry_msgs::Twist& msg){
-//   // int data = msg.linear.x;
-//   ROS_INFO("I heard: [%f]", msg.linear.x);
 
-
-// }
-// // %EndTag(CALLBACK)%
-
-// int main(int argc, char **argv)
-// {
-//   *
-//    * The ros::init() function needs to see argc and argv so that it can perform
-//    * any ROS arguments and name remapping that were provided at the command line.
-//    * For programmatic remappings you can use a different version of init() which takes
-//    * remappings directly, but for most command-line programs, passing argc and argv is
-//    * the easiest way to do it.  The third argument to init() is the name of the node.
-//    *
-//    * You must call one of the versions of ros::init() before using any other
-//    * part of the ROS system.
-   
-//   ros::init(argc, argv, "average_vel_sub");
-//   ros::init(argc, argv, "average_vel_pub");
-
-//   ros::NodeHandle n;
-
-// // %Tag(SUBSCRIBER)%
-//   ros::Subscriber sub = n.subscribe("/turtle1/cmd_vel", 1000, chatterCallback);
-
-//   ros::Publisher vel_pub = n.advertise<std_msgs::float32>("/average_velocity", 1000);
-  
-
-// // %EndTag(SUBSCRIBER)%
-
-// // %Tag(SPIN)%
-//   ros::spin();
-// // %EndTag(SPIN)%
-
-//   return 0;
-// }
-// %EndTag(FULLTEXT)%
