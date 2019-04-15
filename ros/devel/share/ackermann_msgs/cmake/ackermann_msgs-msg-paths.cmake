91ba6da14dac47db152033a85084891554a6724f
NathanJewell <ncjewell@gmail.com>
2019-04-14 17:57:10 -0700

    fix ignore

--- ros/devel/share/ackermann_msgs/cmake/ackermann_msgs-msg-paths.cmake
+++
@@ -1,4 +0,0 @@
-# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
-
-set(ackermann_msgs_MSG_INCLUDE_DIRS "/home/nathan/pemdas/ros/src/system/ackermann_msgs/msg")
-set(ackermann_msgs_MSG_DEPENDENCIES std_msgs)
--- ros/devel/share/vesc_msgs/cmake/vesc_msgs-msg-extras.cmake
+++
@@ -1,2 +0,0 @@
-set(vesc_msgs_MESSAGE_FILES "/home/nathan/pemdas/ros/src/system/vesc/vesc_msgs/msg/VescState.msg;/home/nathan/pemdas/ros/src/system/vesc/vesc_msgs/msg/VescStateStamped.msg")
-set(vesc_msgs_SERVICE_FILES "")
