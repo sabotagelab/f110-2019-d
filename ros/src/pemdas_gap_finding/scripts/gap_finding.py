ae3e707e6593965ec6690b484df4302ab66d8c08
NathanJewell <ncjewell@gmail.com>
2019-04-16 13:21:43 -0700

    msgs publish + restructuring for python pkg

--- ros/src/pemdas_gap_finding/scripts/gap_finding.py
+++ ros/src/pemdas_gap_finding/scripts/gap_finding.py
@@ -3,7 +3,7 @@
 import rospy
 from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import Point
-from pemdas_gap_finding.msg import Gaps
+from pemdas_gap_finding.msg import Gaps, Gap
 
 #publish all gaps to lidar_gaps
 #publish best point to gap_center
@@ -14,7 +14,7 @@ class Interface:
 
         self.sub = rospy.Subscriber("/scan", LaserScan, self.callback)
         self.gapPub = rospy.Publisher("/lidar_gaps", Gaps, queue_size=10)
-	self.pointPub = rospy.Publisher("/center_point", Point, queue_size=100)
+        self.pointPub = rospy.Publisher("/center_point", Point, queue_size=100)
         self.rate = rospy.Rate(rate)
         
 
@@ -23,32 +23,30 @@ class Interface:
 
 
     def callback(self, scanData):
-        gaps = findGaps(scanData.data)
-        gapsMessage = makeGapsMessage(gaps, scanData.data)
-        centerPoint = chooseCenterPoint(gaps, scanData.data)
+        gaps = findGaps(scanData)
+        gapsMessage = makeGapsMessage(gaps, scanData)
+        centerPoint = chooseCenterPoint(gaps, scanData)
         centerPointMessage = makeCenterPointMessage(centerPoint)
         self.gapPub.publish(gapsMessage)
-        self.pointPub.publish(centerPoint)
+        self.pointPub.publish(centerPointMessage)
+        rospy.loginfo("Published Messages")
         self.rate.sleep()
 	
 
 def findGaps(scanData):
-    return []
+    msg = Gap([], 0, 60)
+    return [msg]
 
 def makeGapsMessage(gaps, scan):
-	msg = Gaps()
-	msg.gaps = gaps
-	msg.angle_increment = scan.angle_increment
+    msg = Gaps(gaps, scan.angle_increment)
+    return msg
 
 #find center point in global coords from gaps
 def chooseCenterPoint(gaps, scan):
 	return (0, 0, 0)
 
 def makeCenterPointMessage(centerPoint):
-    msg = Point()
-    msg.x = centerPoint[0]
-    msg.y = centerPoint[1]
-    msg.z = centerPoint[2]
+    msg = Point(*centerPoint)
     return msg
 
 if __name__ == "__main__":
