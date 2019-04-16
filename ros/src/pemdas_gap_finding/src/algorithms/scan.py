bee7ac55af6747ee3c3297ceabf3bf8154af39b5
osboxes.org <bees_inthegarden@yahoo.co.in>
2019-04-16 19:12:20 -0400

    modified kmeans

--- ros/src/pemdas_gap_finding/src/algorithms/scan.py
+++ ros/src/pemdas_gap_finding/src/algorithms/scan.py
@@ -10,21 +10,28 @@ import matplotlib.pyplot as plt
 #import seaborn as sns; sns.set()
 from pemdas_gap_finding.msg import Gap
 
-def callback(msg):
+def findGaps(msg, k=3):
     #make a list to hold data with x = range, y = angle
     X = getData(msg)
-    kmeans = KMeans(n_clusters = 5, init='random').fit(X)
+    print('Running KMeans.....')
+    kmeans = KMeans(n_clusters = k, init='random').fit(X)
     centers = kmeans.cluster_centers_
+    print('Centers found:')
     print(centers)
     #y_kmeans = kmeans.predict(X)
-    # plt.scatter(X[:, 0], X[:, 1], s=50)
-    # plt.scatter(centers[:, 0], centers[:, 1], c='black', s=200, alpha=0.5)
-    # plt.show()
+    #plt.scatter(X[:, 0], X[:, 1], s=50)
+    #plt.scatter(centers[:, 0], centers[:, 1], c='black', s=200, alpha=0.5)
+    #plt.show()
 
+    #print(X[np.where(kmeans.labels_ == 0)])
 
-    # for i in range(5):
-    #     print(np.where(kmeans.labels_ == i)[0, :]) #min
-    #     print(np.where(kmeans.labels_ == i)[-1, :]) #max
+    #list structure
+    #[ [cluster0 min range, cluster avg, cluster0 max range] [...] ]
+    gaps = []
+    for i in range(k):
+        gaps.append([ X[np.where(kmeans.labels_ == i)][0, :].tolist(), centers[i].tolist(), X[np.where(kmeans.labels_ == i)][-1, :].tolist()])
+
+    return(gaps)
 
 
 def getData(msg):
@@ -34,7 +41,8 @@ def getData(msg):
         #print(msg.ranges[i], (i*0.25))
 
     data = np.array(data)
-    data[np.isinf(data)] = 100
+    #replace inf values in data with a value just above max range
+    data[np.isinf(data)] = msg.range_max * 1.02
     #data = np.nan_to_num(data)
     return(data)
 
