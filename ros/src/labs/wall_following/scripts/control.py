e1d9cb2ccb1da244bc65f13cbdb490b329b69ed7
NathanJewell <ncjewell@gmail.com>
2019-04-25 18:23:41 -0700

    fix custom msg generation in wall_following

--- ros/src/labs/wall_following/scripts/control.py
+++ ros/src/labs/wall_following/scripts/control.py
@@ -4,7 +4,7 @@ import rospy
 from race.msg import drive_param
 from std_msgs.msg import Float64
 from wall_following.msg import pid_angle_input
-from collections import queue
+from collections import deque 
 import numpy as np
 
 
@@ -31,23 +31,23 @@ angleLimitFunc = lambda angle : (A*math.exp(C*R) + B * math.exp(R*angle))/(math.
 # data: the PID error from pid_error_node, published as a Float64
 class Interface:
 
-	def __init__(self, errWindowLen):
+	def __init__(self, errWindowLen, angleWindowLen):
 		rospy.init_node('pid_controller_node', anonymous=True)
 
 		self.drivePub = rospy.Publisher('drive_parameters', drive_param, queue_size=1)
 		self.errSub = rospy.Subscriber("pid_error", pid_angle_input, control_callback)
 
-		self.errorWindow = queue(maxlen=errWindowLen)
+		self.errorWindow = deque(maxlen=errWindowLen)
 		self.etInt = 0
 		self.weights = np.asarray(map(weightFunc), xrange(errWindowLen))
 
-		self.angleWindow = queue(maxlen=angleWindowLen)
+		self.angleWindow = deque(maxlen=angleWindowLen)
 
 	def start(self):
 		rospy.spin()
 
 	def storeQ(self, q, elem):
-		if not len(q) < q.maxlen
+		if not len(q) < q.maxlen:
 			q.pop()
 		q.appendleft(elem)
 	
@@ -90,6 +90,6 @@ class Interface:
 # Boilerplate code to start this ROS node.
 # DO NOT MODIFY!
 if __name__ == '__main__':
-	iface = Interface(0, 10, 5)
+	iface = Interface(10, 5)
 	iface.start()
 
