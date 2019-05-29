43fd9ab9e5346f414f81788cbc6956a8d517c48e
osboxes.org <bees_inthegarden@yahoo.co.in>
2019-05-28 20:28:27 -0400

    partial implementation of pure_pursuit

--- ros/src/labs/lab_pure_pursuit/scripts/pure_pursuit.py
+++ ros/src/labs/lab_pure_pursuit/scripts/pure_pursuit.py
@@ -8,14 +8,14 @@ import numpy as np
 from numpy import linalg as LA
 from tf.transformations import euler_from_quaternion, quaternion_from_euler
 import csv
-import os 
+import os
 
 #############
 # CONSTANTS #
 #############
 
-LOOKAHEAD_DISTANCE = 2.0 # meters
-VELOCITY = 1.0 # m/s
+LOOKAHEAD_DISTANCE = 1.5 # meters
+VELOCITY = 0.5 # m/s
 
 
 ###########
@@ -30,7 +30,7 @@ with open(filename) as f:
 
 # Turn path_points into a list of floats to eliminate the need for casts in the code below.
 path_points = [(float(point[0]), float(point[1]), float(point[2])) for point in path_points]
-        
+
 # Publisher for 'drive_parameters' (speed and steering angle)
 pub = rospy.Publisher('drive_parameters', drive_param, queue_size=1)
 
@@ -38,7 +38,7 @@ pub = rospy.Publisher('drive_parameters', drive_param, queue_size=1)
 #############
 # FUNCTIONS #
 #############
-    
+
 # Computes the Euclidean distance between two 2D points p1 and p2.
 def dist(p1, p2):
     return np.sqrt((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2)
@@ -51,32 +51,56 @@ def callback(data):
 
     # 1. Determine the current location of the vehicle (we are subscribed to vesc/odom)
     # Hint: Read up on PoseStamped message type in ROS to determine how to extract x, y, and yaw.
-    
 
-        
+    #rospy.init_node('pure_pursuit_node', anonymous=True)
+    #drivePub = rospy.Publisher('drive_parameters', drive_param, queue_size=1)
+    #PoseSub = rospy.Subscriber("/vesc/odom", PoseStamped, self.callback)
+
+    #get x, y and yaw
+    x = data.pose.position.x
+    y = data.pose.position.y
+
+    quaternion = (
+        data.pose.orientation.x,
+        data.pose.orientation.y,
+        data.pose.orientation.z,
+        data.pose.orientation.w)
+
+    euler = tf.transformations.euler_from_quaternion(quaternion)
+    yaw = euler[2]
+
 
     # 2. Find the path point closest to the vehicle that is >= 1 lookahead distance from vehicle's current location.
+    min_dist = 50
+    for i in range(len(path_points)):
+        d = dist([x, y], path_points[i])
+        if d >= 1:
+            if d < min_dist:
+                min_dist = d
+                index = i
+
+    #path_points[index] is the point closest to the vehicle
 
 
 
-    # 3. Transform the goal point to vehicle coordinates. 
-    
-    
+    # 3. Transform the goal point to vehicle coordinates.
+
+
+
 
     # 4. Calculate the curvature = 1/r = 2x/l^2
     # The curvature is transformed into steering wheel angle by the vehicle on board controller.
     # Hint: You may need to flip to negative because for the VESC a right steering angle has a negative value.
 
-    
+
     angle = np.clip(angle, -0.4189, 0.4189) # 0.4189 radians = 24 degrees because car can only turn 24 degrees max
 
     msg = drive_param()
     msg.velocity = VELOCITY
     msg.angle = angle
     pub.publish(msg)
-    
+
 if __name__ == '__main__':
     rospy.init_node('pure_pursuit')
     rospy.Subscriber('/pf/viz/inferred_pose', PoseStamped, callback, queue_size=1)
     rospy.spin()
-
